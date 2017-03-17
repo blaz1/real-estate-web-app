cc3be2fafab816094ba60c0b0f1b5537e62eb334
blaz1 <artac.blaz@gmail.com>
2017-03-17 16:03:24 +0100

    new boilerplate project with angularJS and MongoDB. Build tool is Maven

---
+++ src/main/java/ftn/sct/RealEstateWebAppApplication.java
@@ -0,0 +1,12 @@
+package ftn.sct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateWebAppApplication.class, args);
+	}
+}
---
+++ src/test/java/ftn/sct/RealEstateWebAppApplicationTests.java
@@ -0,0 +1,16 @@
+package ftn.sct;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RealEstateWebAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
